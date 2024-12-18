b75c9e036a2fb88a1c64a30dfd527c38eb28ed5b
nuttylmao <76841561+nuttylmao@users.noreply.github.com>
2024-12-19 03:45:13 +1100

    Add files via upload

--- script.js
+++ script.js
@@ -4,8 +4,10 @@
 
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
+const followMode = urlParams.has("followMode");
 const twitchUsername = urlParams.get("username") || '';
-const goal = urlParams.get("goal") || 'SUB GOAL';
+const defaultGoalLabel = !followMode ? 'SUB GOAL' : 'FOLLOW GOAL'
+const goal = urlParams.get("goal") || defaultGoalLabel;
 
 ///////////////
 // FUNCTIONS //
@@ -13,15 +15,18 @@ const goal = urlParams.get("goal") || 'SUB GOAL';
 
 async function UpdateMetrics() {
     document.getElementById("goalLabel").innerHTML = `${goal}: `;
-    document.getElementById("subCountLabel").innerHTML = await GetSubCount();
+    if (!followMode)
+        document.getElementById("subCountLabel").innerHTML = await GetSubCount(`https://decapi.me/twitch/subcount`);
+    else
+        document.getElementById("subCountLabel").innerHTML = await GetSubCount(`https://decapi.me/twitch/followcount`);
 
     setTimeout(UpdateMetrics, 10000);
 }
 
 UpdateMetrics();
 
-async function GetSubCount() {
-    const response = await fetch(`https://decapi.me/twitch/subcount/${twitchUsername}`);
+async function GetSubCount(url) {
+    const response = await fetch(`${url}/${twitchUsername}`);
     const metric = await response.text();
 
     if (metric.includes("decapi.me"))
